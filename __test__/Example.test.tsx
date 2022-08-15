0d44e1f2b3eb558947031dfc3a6669cf5112f4dd
Rabii Luena <itisrabii@gmail.com>
2022-08-15 15:52:26 +0300

    chore: adding tests for repo

---
+++ __test__/Example.test.tsx
@@ -0,0 +1,9 @@
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+test.only('testing should work', () => {
+  expect(true).toBeTruthy();
+});
+
+export {};
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  '*.(ts|tsx)': 'npm run validate',
+  '**/*.(ts|tsx)': 'npm run validate',
 };
